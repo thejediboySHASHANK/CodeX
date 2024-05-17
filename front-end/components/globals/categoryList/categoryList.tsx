36563aaf6f910006c72a0ddf10b78a3e4d9e9268
Shashank Dewangan <21052358@kiit.ac.in>
2024-05-17 12:11:15 +0530

    Responsive Optimizations

--- front-end/components/globals/categoryList/categoryList.tsx
+++ front-end/components/globals/categoryList/categoryList.tsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import styles from "./categotyList.module.css"
+import Link from "next/link";
+import Image from "next/image";
+
+import Style from "../../../public/style.png"
+import Fashion from "../../../public/fashion.png"
+import {BookCopy, Brain, Code, Coins, Component, Heart, icons} from "lucide-react";
 
 const CategoryList = () => {
+
+    const categories = [
+        {
+            name: "DSA",
+            href: "/blog?cat=style",
+            imageSrc: Style,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <Code  />
+        },
+        {
+            name: "AI",
+            href: "/blog",
+            imageSrc: Fashion,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <Brain />
+        },
+        {
+            name: "Blockchain",
+            href: "/blog",
+            imageSrc: Style,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <BookCopy />
+        },
+        {
+            name: "Design",
+            href: "/blog",
+            imageSrc: Style,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <Component />
+        },
+        {
+            name: "Startup",
+            href: "/blog",
+            imageSrc: Style,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <Coins />
+        },
+        {
+            name: "Lifestyle",
+            href: "/blog",
+            imageSrc: Style,
+            imageAlt: "Style",
+            imageWidth: 32,
+            imageHeight: 32,
+            icons: <Heart />
+        },
+    ]
+
     return (
-        <div className={styles.container}>CategoryList</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Categories</h1>
+            <div className={styles.categories}>
+                {categories.map((category) => (
+                    <Link href={category.href} key={category.name} className={`${styles.category} ${styles[category.name.toLowerCase()]}`}>
+                        {/*<Image*/}
+                        {/*    src={category.imageSrc}*/}
+                        {/*    alt={category.imageAlt}*/}
+                        {/*    width={category.imageWidth}*/}
+                        {/*    height={category.imageHeight}*/}
+                        {/*    className={styles.image}*/}
+                        {/*/>*/}
+                        {category.icons}
+                        {category.name}
+                    </Link>
+                ))}
+            </div>
+        </div>
     )
 }
 export default CategoryList
